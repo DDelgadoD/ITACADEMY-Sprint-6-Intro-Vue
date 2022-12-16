a713f67d80bc1cb88a6b11f7aebb5fb2beeddbc0
DDelgadoD <david.delgado82@gmail.com>
2022-12-16 17:59:51 +0100

    Exercici 2

--- src/components/Escena.vue
+++ src/components/Escena.vue
@@ -1,7 +1,12 @@
 <template>
-    "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial".
+    <div v-for="sentence in data.sentences">
+        <div>
+            {{ sentence }}
+        </div>
+    </div>
 </template>
 <script setup>
+    import data from "../data/data.json"
 </script>
 <style scoped>
 </style>
