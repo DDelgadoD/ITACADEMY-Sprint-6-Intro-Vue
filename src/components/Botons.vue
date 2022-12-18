ff873c7b3e1c64321220f5707c5ebf8197ac9b2d
DDelgadoD <david.delgado82@gmail.com>
2022-12-18 20:04:20 +0100

    Nivell 2 Acabat

--- src/components/Botons.vue
+++ src/components/Botons.vue
@@ -4,15 +4,12 @@
         <button class="p-4 flex-fill" @click="move(currentSentence, -1)">Següent</button>
     </div>
 </template>
+
 <script setup>
-
-const props = defineProps({
-    currentSentence: Array
-});
-
-const move = (arr, dir=1) => dir >= 0 ?  arr.push(arr.shift()) : arr.unshift(arr.pop()); 
-
+    const props = defineProps({ currentSentence: Array });
+    const move = (arr, dir=1) => dir >= 0 ?  arr.push(arr.shift()) : arr.unshift(arr.pop()); 
 </script>
+
 <style scoped>
 
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,19 @@
 <template>
-  <Botons :currentSentence="currentSentence"/>
-  <Escena :sentences="data.sentences" :currentSentence="currentSentence" />
+  <div v-if="isShowed">
+    <div class="flex flex-column mt-5 border border-dark mx-5 w-50 mx-auto shadow-lg">
+      <div class="m-5">
+        A continuació podrás veure un tutorial, en el qual, mitjançant dos botons els nous usuaris poden avançar o
+        retrocedir en els consells, modificant-se el text d'ajuda i la imatge de fons.
+      </div>
+      <div class="d-flex justify-content-center">
+        <button class="btn btn-success btn-lg rounded-pill my-4 " @click="isShowed = !isShowed"> D'accord </button>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <Botons :currentSentence="currentSentence" />
+    <Escena :sentences="data.sentences" :currentSentence="currentSentence" />
+  </div>
 </template>
 
 <script setup>
@@ -9,8 +22,8 @@ import Escena from './Escena.vue';
 import data from "../data/data.json";
 import { ref } from 'vue';
 
-const currentSentence = ref([1,0,0,0]);
-
+const currentSentence = ref([1, 0, 0, 0]);
+const isShowed = ref(true);
 
 </script>
 
